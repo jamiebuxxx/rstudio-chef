d39d5c2a7b97c64cd5f90feb537a8ee1371a3f82
JaMile Jackson <jjackson@gracenote.com>
2015-12-09 11:56:00 -0800

    use nginx package install instead of cookbook

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -1,4 +1,9 @@
-include_recipe "nginx"
+package "nginx"
+
+service 'nginx' do
+  supports status: true, restart: true, reload: true
+  action [:enable, :start]
+end
 
 # Only configure Nginx if the server_name attribute has been set.
 if node['rstudio']['nginx']['server_name'] == ''
